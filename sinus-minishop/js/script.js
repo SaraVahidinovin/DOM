ef4c75bfb68a6197734074a7b9b85cf213785889
unknown <sa.vahidinovin@gmail.com>
2024-07-10 11:18:50 +0200

    exercises1-4

--- sinus-minishop/js/script.js
+++ sinus-minishop/js/script.js
@@ -1,1 +1,18 @@
 // Code here!
+//1. Rename the first hoodie from Ash to Potato .
+
+/*const hoodieTitle = document.querySelector('.art-1 h3');
+hoodieTitle.innerHTML = 'Potato';*/
+
+document.querySelector('.art-1 h3').innerHTML = 'Potato';
+
+//2. Rename Home to Start .​​
+document.querySelector('nav a:first-child').innerHTML = 'Start';
+
+//3. Rename Contact to Mail Us .​
+document.querySelector('nav a:nth-child(3)').innerHTML = 'Mail Us';
+
+//4. Replace the information on Sinus Hoodie - Fire .
+document.querySelector('.art-2 h2').innerHTML = 'New Sinus Hoodie';
+document.querySelector('.art-2 h3').innerHTML = 'New Fire';
+document.querySelector('.art-2 p').innerHTML = 'This is the updated description for the Sinus Hoodie - Fire. It is hotter than ever!';
